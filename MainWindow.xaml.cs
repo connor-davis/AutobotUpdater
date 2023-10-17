49aaf7db6e49e534faaa9ceecc96136ac1498276
Connor Davis <cnnrproton@gmail.com>
2023-10-17 17:09:15 +0200

    Clean it up a bit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,8 +40,7 @@ namespace AutobotUpdater
             var gitHubClient = new GitHubClient(new ProductHeaderValue("connor-davis"));
             var releases = await gitHubClient.Repository.Release.GetAll("connor-davis", "Autobotv4");
             var latestGithubVersion = new Version(releases[0].TagName.Replace("v", ""));
-            var downloadUrl =
-                $"https://github.com/connor-davis/Autobotv4/releases/download/v{latestGithubVersion}/Autobotv4.zip";
+            var downloadUrl = $"https://github.com/connor-davis/Autobotv4/releases/download/v{latestGithubVersion}/Autobotv4.zip";
 
             using var httpClient = new HttpClient();
 
@@ -55,7 +54,7 @@ namespace AutobotUpdater
 
                 // Check if the response is successful
                 response.EnsureSuccessStatusCode();
-
+                
                 // Get the content length (file size) from the response headers
                 var totalBytes = response.Content.Headers.ContentLength.GetValueOrDefault();
 
@@ -97,7 +96,12 @@ namespace AutobotUpdater
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                DownloadStatus.Text = "Please wait an hour before updating again. You have reached your limit.";
+                DownloadProgressBar.Visibility = Visibility.Collapsed;    
+                
+                Thread.Sleep(2000);
+                    
+                Environment.Exit(0);
             }
         }
 
@@ -144,78 +148,6 @@ namespace AutobotUpdater
 
                 Console.WriteLine($"Error: {ex.Message}");
             }
-        }
-
-        private static bool CopyUpdate(string sourceDir, string targetDir)
-        {
-            if (!Directory.Exists(targetDir))
-            {
-                Directory.CreateDirectory(targetDir);
-            }
-
-            var files = Directory.GetFiles(sourceDir);
-
-            foreach (var file in files)
-            {
-                var fileName = Path.GetFileName(file);
-                var destFile = Path.Combine(targetDir, fileName);
-
-                try
-                {
-                    if (File.Exists(destFile))
-                    {
-                        File.Delete(destFile);
-                        File.Copy(file, destFile);
-                    }
-                    else
-                    {
-                        File.Copy(file, destFile);
-                    }
-                }
-                catch
-                {
-                    // ignore
-                }
-            }
-
-            var subdirectories = Directory.GetDirectories(sourceDir);
-
-            foreach (var subDir in subdirectories)
-            {
-                var subDirName = new DirectoryInfo(subDir).Name;
-                var destSubDir = Path.Combine(targetDir, subDirName);
-
-                CopyUpdate(subDir, destSubDir);
-            }
-
-            return true;
-        }
-
-        private static bool DeleteUpdate(string sourceDir)
-        {
-            var files = Directory.GetFiles(sourceDir);
-
-            foreach (var file in files)
-            {
-                var fileName = Path.GetFileName(file);
-                var destFile = Path.Combine(sourceDir, fileName);
-                
-                File.Delete(destFile);
-            }
-
-            var subdirectories = Directory.GetDirectories(sourceDir);
-
-            foreach (var subDir in subdirectories)
-            {
-                var subDirName = new DirectoryInfo(subDir).Name;
-                var destSubDir = Path.Combine(sourceDir, subDirName);
-
-                DeleteUpdate(destSubDir);
-            }
-            
-            Directory.Delete(sourceDir);
-            
-            return true;
         }
     }
 }
